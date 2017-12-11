676cf98258b55b493dee5cdd6c8b0a94d1b89e2e
Manuela Aru <33028420+manuelaweb@users.noreply.github.com>
2017-12-11 15:17:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include('head.php'); ?>
+  
+</head>
+<body>
+
+ <div class="container">
+    <div class="row riga-1">
+    <div class="col-sm">
+      <?php include('navbar.php'); ?>
+    </div>
+  </div>
+
+    <div class="row riga-2">
+    <div class="col-md-3">
+      <?php include('sidebar.php'); ?>
+    </div>
+    <div class="col-md-9">
+      <?php include('home.php'); ?>
+    </div>
+  </div>
+    
+    <div class="row riga-3">
+    <div class="col-sm">
+      Footer
+    </div>
+  </div>
+
+</div>
+
+</body>
+</html>
